83e6003a66f3e252446b5b622eae635660d7ac98
aoizora <mathesis@inbox.ru>
2024-05-14 16:47:06 +0200

    Документировал API

--- src/main/java/aoizora/controller/MailingEndpoint.java
+++ src/main/java/aoizora/controller/MailingEndpoint.java
@@ -3,12 +3,15 @@ package aoizora.controller;
 import aoizora.converter.MailingConverter;
 import aoizora.model.MailingDTO;
 import aoizora.service.MailingService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Api(value = "API для работы с почтовыми отправлениями", produces = "application/json")
 @Controller
 @RequestMapping("/api/mailing")
 public class MailingEndpoint
@@ -23,8 +26,9 @@ public class MailingEndpoint
         this.mailingConverter = mailingConverter;
     }
 
+    @ApiOperation(value = "Зарегистрировать почтовое отправление", produces = "application/json")
     @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody MailingDTO mailing)
+    public ResponseEntity<MailingDTO> register(@RequestBody MailingDTO mailing)
     {
         return ResponseEntity.ok(mailingConverter.convert(mailingService.register(
                 mailing.getRecipient(),
